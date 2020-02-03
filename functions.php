ac87c64bf5e4682fb5cf875cf445918a48eae286
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-02-03 15:20:21 +0300

    Init

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Hypermarket theme functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * Do not add any custom code here.
+ * Please use a custom plugin or child theme so that your customizations aren't lost during updates.
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ *
+ * @see 		https://codex.wordpress.org/Theme_Development
+ * @see 		https://codex.wordpress.org/Plugin_API
+ * @since 	    2.0.0
+ * @package 	hypermarket
+ * @author  	MyPreview (Github: @mahdiyazdani, @mypreview)
+ */
