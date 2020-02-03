70dad4c0b1832e18c176031df06c2f8310036c24
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-02-03 15:50:47 +0300

    Added template for displaying sidebar area

---
+++ sidebar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link 		https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ * @since 	    2.0.0
+ * @package 	hypermarket
+ * @author  	MyPreview (Github: @mahdiyazdani, @mypreview)
+ */
+
+if ( ! is_active_sidebar( 'sidebar' ) || apply_filters( 'hypermarket_disable_sidebar', FALSE ) ) {
+	return;
+} // End If Statement
+
+?><div id="secondary" class="widget-area" role="complementary"><?php 
+	dynamic_sidebar( 'sidebar' ); 
+?></div><!-- #secondary -->
