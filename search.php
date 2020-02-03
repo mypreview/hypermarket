adc0f2a4729eb65f20bb3a5aac096b8ed0a5fba6
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-02-04 00:20:04 +0300

    Added template for displaying search results pages

---
+++ search.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying search results pages.
+ *
+ * @link 		https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ * @since 	    2.0.0
+ * @package 	hypermarket
+ * @author  	MyPreview (Github: @mahdiyazdani, @mypreview)
+ */
+
+get_header(); 
+
+	?><div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main"><?php 
+
+		if ( have_posts() ) : 
+
+			?><header class="page-header">
+				<h1 class="page-title"><?php
+					/* translators: %s: search term */
+					printf( esc_attr__( 'Search Results for: %s', 'hypermarket' ), sprintf( '<span>%s</span>', get_search_query() ) );
+				?></h1>
+			</header><!-- .page-header --><?php
+
+			get_template_part( 'loop' );
+		else :
+			get_template_part( 'content', 'none' );
+		endif;
+		
+		?></main><!-- #main -->
+	</div><!-- #primary --><?php
+
+	do_action( 'hypermarket_sidebar' );
+
+get_footer();
