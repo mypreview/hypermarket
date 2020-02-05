e083d94367788b7ca96d711c782517f5aa19ab4b
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-02-06 00:40:02 +0300

    Display WooCommerce shop messages

--- inc/woocommerce/hypermarket-woocommerce-template-functions.php
+++ inc/woocommerce/hypermarket-woocommerce-template-functions.php
@@ -75,3 +75,76 @@ if ( ! function_exists( 'hypermarket_cart_link' ) ) :
 		</a><?php
 	}
 endif;
+
+if ( ! function_exists( 'hypermarket_header_cart' ) ) :
+	/**
+	 * Display Header Cart
+	 *
+	 * @return 	void
+	 */
+	function hypermarket_header_cart() {
+		if ( hypermarket_is_woocommerce_activated() ) {
+			if ( is_cart() ) {
+				$class = 'current-menu-item';
+			} else {
+				$class = '';
+			} // End If Statement
+			?><ul id="site-header-cart" class="site-header-cart menu">
+				<li class="<?php echo esc_attr( $class ); ?>"><?php 
+					hypermarket_cart_link(); 
+				?></li>
+				<li><?php 
+					the_widget( 'WC_Widget_Cart', 'title=' );
+				?></li>
+			</ul><?php
+		}
+	}
+endif;
+
+if ( ! function_exists( 'hypermarket_upsell_display' ) ) :
+	/**
+	 * Upsells
+	 * Replace the default upsell function with our own which displays the correct number product columns
+	 *
+	 * @return  void
+	 */
+	function hypermarket_upsell_display() {
+		$columns = apply_filters( 'hypermarket_upsells_columns', 3 );
+		woocommerce_upsell_display( -1, $columns );
+	}
+endif;
+
+if ( ! function_exists( 'hypermarket_sorting_wrapper' ) ) :
+	/**
+	 * Sorting wrapper
+	 *
+	 * @return  void
+	 */
+	function hypermarket_sorting_wrapper() {
+		echo '<div class="hypermarket-sorting">';
+	}
+endif;
+
+if ( ! function_exists( 'hypermarket_sorting_wrapper_close' ) ) :
+	/**
+	 * Sorting wrapper close
+	 *
+	 * @return  void
+	 */
+	function hypermarket_sorting_wrapper_close() {
+		echo '</div>';
+	}
+endif;
+
+if ( ! function_exists( 'hypermarket_shop_messages' ) ) :
+	/**
+	 * WooCommerce shop messages
+	 *
+	 * @return  void
+	 */
+	function hypermarket_shop_messages() {
+		if ( ! is_checkout() ) {
+			echo wp_kses_post( hypermarket_do_shortcode( 'woocommerce_messages' ) ); // WPCS: XSS ok.
+		}
+	}
+endif;
