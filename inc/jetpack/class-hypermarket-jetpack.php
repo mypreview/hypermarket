20725c5626d4fd523ba9e60342b58bd6182c8ace
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-02-05 18:28:06 +0300

    Created Jetpack integration class

---
+++ inc/jetpack/class-hypermarket-jetpack.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Hypermarket Jetpack Class
+ *
+ * @since 	    2.0.0
+ * @package 	hypermarket
+ * @author  	MyPreview (Github: @mahdiyazdani, @mypreview)
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; 
+} // End If Statement
+
+if ( ! class_exists( 'Hypermarket_Jetpack' ) ) :
+
+	/**
+	 * The main `Hypermarket` Jetpack integration class
+	 */
+	final class Hypermarket_Jetpack {
+
+		/**
+		 * Setup class.
+		 * 
+		 * @return  void
+		 */
+		public function __construct() {
+			add_action( 'after_setup_theme', array( $this, 'setup' ) );
+		}
+
+		/**
+		 * Theme support for content options.
+		 * 
+		 * @return 	void
+		 */
+		public function setup() {
+			add_theme_support( 
+				'jetpack-content-options', apply_filters( 
+					'hypermarket_jetpack_content_args', array(
+						'blog-display' => array( 'content', 'excerpt' ),
+						'author-bio' 		 => true,
+						'author-bio-default' => true,
+						'post-details' => array(
+							'stylesheet' => 'hypermarket-style',
+							'date' 		 => '.posted-on',
+							'categories' => '.cat-links',
+							'tags' 		 => '.tags-links',
+							'author' 	 => '.byline',
+							'comment' 	 => '.comments-link'
+						),
+						'featured-images' => array(
+							'archive' 	  	  => true,
+							'archive-default' => true,
+							'post' 			  => true,
+							'post-default' 	  => true,
+							'page' 			  => true,
+							'page-default' 	  => true
+						) 
+					) 
+				) 
+			);
+		}
+
+	}
+endif;
+
+return new Hypermarket_Jetpack();
