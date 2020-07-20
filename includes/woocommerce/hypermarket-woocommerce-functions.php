f83b156d7476e9b5b60dc7724d6c7d367af362ce
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-07-20 22:42:49 +0300

    Code tidy + Ran lint

--- includes/woocommerce/hypermarket-woocommerce-functions.php
+++ includes/woocommerce/hypermarket-woocommerce-functions.php
@@ -2,16 +2,20 @@
 /**
  * Hypermarket WooCommerce functions.
  *
- * @since       2.0.0
- * @package     hypermarket
- * @author      MyPreview (Github: @mahdiyazdani, @mypreview)
+ * @link       https://www.upwork.com/fl/mahdiyazdani
+ * @author     Mahdi Yazdani <mahdiyazdani@mail.com>
+ * @since      2.0.0
+ *
+ * @package    hypermarket
+ * @subpackage hypermarket/includes/woocommerce
  */
 
 if ( ! function_exists( 'hypermarket_is_product_archive' ) ) :
 	/**
 	 * Checks if the current page is a product archive
 	 *
-	 * @return  boolean
+	 * @since   1.0.0
+	 * @return  bool
 	 */
 	function hypermarket_is_product_archive() {
 		if ( is_shop() || is_product_taxonomy() || is_product_category() || is_product_tag() ) {
@@ -22,28 +26,34 @@ if ( ! function_exists( 'hypermarket_is_product_archive' ) ) :
 	}
 endif;
 
-/**
- * Retrieves the previous product.
- *
- * @param   bool         $in_same_term       Optional. Whether post should be in a same taxonomy term. Default false.
- * @param   array|string $excluded_terms     Optional. Comma-separated list of excluded term IDs. Default empty.
- * @param   string       $taxonomy           Optional. Taxonomy, if $in_same_term is true. Default 'product_cat'.
- * @return  WC_Product|false                            Product object if successful. False if no valid product is found.
- */
-function hypermarket_get_previous_product( $in_same_term = false, $excluded_terms = '', $taxonomy = 'product_cat' ) {
-	$product = new Hypermarket_WooCommerce_Adjacent_Products( $in_same_term, $excluded_terms, $taxonomy, true );
-	return $product->get_product();
-}
+if ( ! function_exists( 'hypermarket_get_previous_product' ) ) :
+	/**
+	 * Retrieves the previous product.
+	 *
+	 * @since   1.0.0
+	 * @param   bool         $in_same_term       Optional. Whether post should be in a same taxonomy term. Default false.
+	 * @param   array|string $excluded_terms     Optional. Comma-separated list of excluded term IDs. Default empty.
+	 * @param   string       $taxonomy           Optional. Taxonomy, if $in_same_term is true. Default 'product_cat'.
+	 * @return  WC_Product|false                            Product object if successful. False if no valid product is found.
+	 */
+	function hypermarket_get_previous_product( $in_same_term = false, $excluded_terms = '', $taxonomy = 'product_cat' ) {
+		$product = new Hypermarket_WooCommerce_Adjacent_Products( $in_same_term, $excluded_terms, $taxonomy, true );
+		return $product->get_product();
+	}
+endif;
 
-/**
- * Retrieves the next product.
- *
- * @param   bool         $in_same_term       Optional. Whether post should be in a same taxonomy term. Default false.
- * @param   array|string $excluded_terms     Optional. Comma-separated list of excluded term IDs. Default empty.
- * @param   string       $taxonomy           Optional. Taxonomy, if $in_same_term is true. Default 'product_cat'.
- * @return  WC_Product|false                            Product object if successful. False if no valid product is found.
- */
-function hypermarket_get_next_product( $in_same_term = false, $excluded_terms = '', $taxonomy = 'product_cat' ) {
-	$product = new Hypermarket_WooCommerce_Adjacent_Products( $in_same_term, $excluded_terms, $taxonomy );
-	return $product->get_product();
-}
+if ( ! function_exists( 'hypermarket_get_next_product' ) ) :
+	/**
+	 * Retrieves the next product.
+	 *
+	 * @since   1.0.0
+	 * @param   bool         $in_same_term       Optional. Whether post should be in a same taxonomy term. Default false.
+	 * @param   array|string $excluded_terms     Optional. Comma-separated list of excluded term IDs. Default empty.
+	 * @param   string       $taxonomy           Optional. Taxonomy, if $in_same_term is true. Default 'product_cat'.
+	 * @return  WC_Product|false                            Product object if successful. False if no valid product is found.
+	 */
+	function hypermarket_get_next_product( $in_same_term = false, $excluded_terms = '', $taxonomy = 'product_cat' ) {
+		$product = new Hypermarket_WooCommerce_Adjacent_Products( $in_same_term, $excluded_terms, $taxonomy );
+		return $product->get_product();
+	}
+endif;
