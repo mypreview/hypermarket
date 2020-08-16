17804793f4ba7dc241e32cd46b545416c92dce58
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-08-16 17:08:18 +0300

    Trigger the custom scripts once the Customizer is ready and fully loaded

---
+++ src/customize/index.js
@@ -0,0 +1,17 @@
+import './style.css';
+import { range } from './range';
+
+/**
+ * External dependencies
+ */
+import forEach from 'lodash/forEach';
+
+// Bail early, in case the Customizer JavaScript API is not present!
+if ( !! wp && !! wp.customize ) {
+	// Trigger the custom scripts once the Customizer is ready and fully loaded.
+	window.wp.customize.bind( 'ready', () => {
+		forEach( [ range ], ( component ) => {
+			component.ready();
+		} );
+	} );
+}
