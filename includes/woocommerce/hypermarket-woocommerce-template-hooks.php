901bf1230568ed7f9312c33c4656285b040bcd5c
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-01 13:32:16 +0300

    Removed unused hooks

--- includes/woocommerce/hypermarket-woocommerce-template-hooks.php
+++ includes/woocommerce/hypermarket-woocommerce-template-hooks.php
@@ -44,8 +44,6 @@ add_action( 'hypermarket_content_top', 'hypermarket_shop_messages' );
  *
  * @see  hypermarket_before_content()
  * @see  hypermarket_after_content()
- * @see  hypermarket_products_div()
- * @see  hypermarket_div_close()
  */
 remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper' );
@@ -55,11 +53,8 @@ remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination' );
 remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
 remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
 add_action( 'woocommerce_before_main_content', 'hypermarket_before_content' );
-add_action( 'woocommerce_before_main_content', 'hypermarket_before_loop_widget_region', 20 );
 add_action( 'woocommerce_after_main_content', 'hypermarket_after_content' );
-add_action( 'woocommerce_before_shop_loop', 'hypermarket_products_div', 0 );
 add_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 30 );
-add_action( 'woocommerce_after_shop_loop', 'hypermarket_div_close', 30 );
 
 /**
  * Products
@@ -74,7 +69,7 @@ remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_l
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
 remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash' );
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price' );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 add_action( 'woocommerce_single_product_summary', 'hypermarket_edit_post_link', 60 );
 add_action( 'woocommerce_after_add_to_cart_button', 'woocommerce_template_single_price' );
