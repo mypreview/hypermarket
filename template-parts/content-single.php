906017fbcbfd981b0472154a7e59317a92a42745
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-11 01:44:28 +0300

    Updated hooks

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -19,10 +19,9 @@
 		/**
 		 * Functions hooked into `hypermarket_single_post` add_action
 		 *
-		 * @hooked hypermarket_div                    - 10
-		 * @hooked hypermarket_post_thumbnail         - 20
-		 * @hooked hypermarket_post_header            - 30
-		 * @hooked hypermarket_div_close              - 35 
+		 * @hooked hypermarket_post_header            - 10
+		 * @hooked hypermarket_post_meta              - 20
+		 * @hooked hypermarket_post_thumbnail         - 30
 		 * @hooked hypermarket_post_content           - 40
 		 */
 		do_action( 'hypermarket_single_post' );
@@ -31,7 +30,8 @@
 		 * Functions hooked in to `hypermarket_single_post_bottom` action
 		 *
 		 * @hooked hypermarket_edit_post_link         - 10
-		 * @hooked hypermarket_display_comments       - 20
+		 * @hooked hypermarket_post_footnote          - 20
+		 * @hooked hypermarket_display_comments       - 30
 		 */
 		do_action( 'hypermarket_single_post_bottom' );
 
