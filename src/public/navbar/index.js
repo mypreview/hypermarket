512c1a7061f1a238022d0165f196259d309064d8
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-11 02:48:58 +0300

    Avoid sub-menus from going off-screen

---
+++ src/public/navbar/index.js
@@ -0,0 +1,43 @@
+/* global hypermarket */
+
+/**
+ * External dependencies
+ */
+import $ from 'jquery';
+
+export const navbar = {
+	cache() {
+		navbar.els = {};
+		navbar.vars = {};
+		navbar.vars.elm = 'site-navigation';
+		navbar.els.submenu = $( `#${ navbar.vars.elm } .menu-item-has-children` );
+	},
+	// Execute callback after the DOM is loaded.
+	ready() {
+		navbar.cache();
+		navbar.isOffScreen();
+	},
+	// Avoid sub-menus from going off-screen.
+	isOffScreen() {
+		const className = 'is-off-screen';
+		navbar.els.submenu.on( 'mouseenter mouseleave', function () {
+			if ( $( 'ul', this ).length ) {
+				const $window = $( window ),
+					windowWidth = $window.innerWidth(),
+					$submenu = $( 'ul:first', this ),
+					offset = $submenu.offset(),
+					width = $submenu.width(),
+					left = offset.left,
+					right = windowWidth - ( left + width ),
+					isVisible = hypermarket.isRTL ? right + width <= windowWidth : left + width <= windowWidth;
+				if ( ! isVisible ) {
+					$( this ).addClass( className );
+				} else {
+					setTimeout( () => {
+						$( this ).removeClass( className );
+					}, 300 );
+				}
+			}
+		} );
+	},
+};
