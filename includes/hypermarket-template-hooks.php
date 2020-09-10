217ebe89a003bd4ea062250ae621337b63d46016
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-11 01:44:37 +0300

    Updated single post hooks

--- includes/hypermarket-template-hooks.php
+++ includes/hypermarket-template-hooks.php
@@ -49,31 +49,27 @@ add_action( 'hypermarket_sidebar', 'hypermarket_get_sidebar' );
 /**
  * Posts
  *
- * @see  hypermarket_div()
  * @see  hypermarket_post_thumbnail()
  * @see  hypermarket_post_header()
- * @see  hypermarket_posted_by()
- * @see  hypermarket_posted_on()
- * @see  hypermarket_post_taxonomy()
- * @see  hypermarket_post_content()
  * @see  hypermarket_paging_nav()
+ * @see  hypermarket_post_header()
+ * @see  hypermarket_post_meta()
+ * @see  hypermarket_post_content()
+ * @see  hypermarket_edit_post_link()
+ * @see  hypermarket_post_footnote()
  * @see  hypermarket_display_comments()
- * @see  hypermarket_div_close()
  * @see  hypermarket_post_excerpt()
  */
-add_action( 'hypermarket_loop_post', 'hypermarket_post_thumbnail', 10, 2 );
+add_action( 'hypermarket_loop_post', 'hypermarket_post_thumbnail' );
 add_action( 'hypermarket_loop_post', 'hypermarket_post_header', 20 );
 add_action( 'hypermarket_loop_after', 'hypermarket_paging_nav' );
-add_action( 'hypermarket_single_post', 'hypermarket_div' );
-add_action( 'hypermarket_single_post', 'hypermarket_post_thumbnail', 20, 2 );
-add_action( 'hypermarket_single_post', 'hypermarket_post_header', 30 );
-add_action( 'hypermarket_single_post', 'hypermarket_div_close', 35 );
+add_action( 'hypermarket_single_post', 'hypermarket_post_header' );
+add_action( 'hypermarket_single_post', 'hypermarket_post_meta', 20 );
+add_action( 'hypermarket_single_post', 'hypermarket_post_thumbnail', 30 );
 add_action( 'hypermarket_single_post', 'hypermarket_post_content', 40 );
 add_action( 'hypermarket_single_post_bottom', 'hypermarket_edit_post_link' );
+add_action( 'hypermarket_single_post_bottom', 'hypermarket_post_footnote' );
 add_action( 'hypermarket_single_post_bottom', 'hypermarket_display_comments', 20 );
-add_action( 'hypermarket_post_meta', 'hypermarket_posted_by' );
-add_action( 'hypermarket_post_meta', 'hypermarket_posted_on', 20 );
-add_action( 'hypermarket_post_meta', 'hypermarket_post_taxonomy', 30 );
 add_action( 'hypermarket_post_header_after', 'hypermarket_post_excerpt' );
 
 /**
