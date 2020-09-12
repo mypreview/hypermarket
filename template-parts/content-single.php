381ff4e945ee1c9e8f8efda2b38998a45364f700
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-12 23:01:17 +0300

    Typo

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying posts.
+ * Template part for displaying post content.
  *
  * @link        https://developer.wordpress.org/themes/basics/template-hierarchy/
  * @link        https://www.upwork.com/fl/mahdiyazdani
