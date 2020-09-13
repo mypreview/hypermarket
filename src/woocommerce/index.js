ca41448330d34a3782a67b2d7e8c98269bec510a
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-14 01:23:48 +0300

    Imported `handheld` script

--- src/woocommerce/index.js
+++ src/woocommerce/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+import { handheld } from './handheld';
+
+/**
+ * External dependencies
+ */
+import forEach from 'lodash/forEach';
+import domReady from '@wordpress/dom-ready';
+
+domReady( () => {
+	forEach( [ handheld ], ( component ) => {
+		component.ready();
+	} );
+} );
