b57864d8b02ad3e54e130d4ff4c1f42a49d78f40
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-15 20:51:33 +0300

    Updated method priorities

--- includes/hypermarket-template-hooks.php
+++ includes/hypermarket-template-hooks.php
@@ -69,12 +69,12 @@ add_action( 'hypermarket_loop_post', 'hypermarket_post_thumbnail', 30 );
 add_action( 'hypermarket_loop_post', 'hypermarket_post_excerpt', 40 );
 add_action( 'hypermarket_loop_post', 'hypermarket_post_footnote', 50 );
 add_action( 'hypermarket_loop_after', 'hypermarket_paging_nav' );
-add_action( 'hypermarket_single_post', 'hypermarket_div', 5 );
-add_action( 'hypermarket_single_post', 'hypermarket_post_header' );
-add_action( 'hypermarket_single_post', 'hypermarket_div_close', 25 );
-add_action( 'hypermarket_single_post', 'hypermarket_post_meta', 30 );
-add_action( 'hypermarket_single_post', 'hypermarket_post_thumbnail', 40 );
-add_action( 'hypermarket_single_post', 'hypermarket_post_content', 50 );
+add_action( 'hypermarket_single_post_top', 'hypermarket_div', 5 );
+add_action( 'hypermarket_single_post_top', 'hypermarket_post_header' );
+add_action( 'hypermarket_single_post_top', 'hypermarket_div_close', 25 );
+add_action( 'hypermarket_single_post', 'hypermarket_post_meta' );
+add_action( 'hypermarket_single_post', 'hypermarket_post_thumbnail', 20 );
+add_action( 'hypermarket_single_post', 'hypermarket_post_content', 30 );
 add_action( 'hypermarket_single_post_bottom', 'hypermarket_edit_post_link' );
 add_action( 'hypermarket_single_post_bottom', 'hypermarket_post_footnote', 20 );
 add_action( 'hypermarket_single_post_bottom', 'hypermarket_post_nav', 30 );
@@ -87,12 +87,16 @@ add_action( 'hypermarket_single_post_bottom', 'hypermarket_display_comments', 40
  * @see  hypermarket_post_thumbnail()
  * @see  hypermarket_page_header()
  * @see  hypermarket_div_close()
- * @see  hypermarket_post_content()
+ * @see  hypermarket_page_content()
+ * @see  hypermarket_edit_post_link()
+ * @see  hypermarket_paging_nav()
  * @see  hypermarket_display_comments()
  */
-add_action( 'hypermarket_page', 'hypermarket_div' );
-add_action( 'hypermarket_page', 'hypermarket_post_thumbnail', 20, 2 );
-add_action( 'hypermarket_page', 'hypermarket_page_header', 30 );
-add_action( 'hypermarket_page', 'hypermarket_div_close', 35 );
-add_action( 'hypermarket_page', 'hypermarket_page_content', 40 );
-add_action( 'hypermarket_page_after', 'hypermarket_display_comments' );
+add_action( 'hypermarket_page_top', 'hypermarket_div', 5 );
+add_action( 'hypermarket_page_top', 'hypermarket_page_header' );
+add_action( 'hypermarket_page_top', 'hypermarket_div_close', 25 );
+add_action( 'hypermarket_page', 'hypermarket_post_thumbnail' );
+add_action( 'hypermarket_page', 'hypermarket_page_content', 20 );
+add_action( 'hypermarket_page_bottom', 'hypermarket_edit_post_link' );
+add_action( 'hypermarket_page_bottom', 'hypermarket_paging_nav', 20 );
+add_action( 'hypermarket_page_bottom', 'hypermarket_display_comments', 30 );
