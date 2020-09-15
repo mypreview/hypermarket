b2774db4234b95ff9820f97d6a5e8bd9ffefc647
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-15 22:00:22 +0300

    Remove tap highlight on Windows Phone IE

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0" />
+	<meta name="msapplication-tap-highlight" content="no" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<?php wp_head(); ?>
 </head>
