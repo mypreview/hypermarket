2700043221505eb1c8ffcbf4c16af03b56bf29e7
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-18 21:30:24 +0300

    Imported `Quantity` script

--- src/woocommerce/index.js
+++ src/woocommerce/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import { handheld } from './handheld';
+import { quantity } from './quantity';
 
 /**
  * External dependencies
@@ -8,7 +9,7 @@ import forEach from 'lodash/forEach';
 import domReady from '@wordpress/dom-ready';
 
 domReady( () => {
-	forEach( [ handheld ], ( component ) => {
+	forEach( [ handheld, quantity ], ( component ) => {
 		component.ready();
 	} );
 } );
