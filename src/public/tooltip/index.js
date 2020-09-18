9adfa1e41d43ab847e40928720d8ac653203104e
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-18 21:04:18 +0300

    Re-initialize tooltip on Ajax event(s) being triggered

--- src/public/tooltip/index.js
+++ src/public/tooltip/index.js
@@ -1,22 +1,30 @@
 /**
  * External dependencies
  */
+import $ from 'jquery';
 import tippy from 'tippy.js';
 
 export const tooltip = {
 	cache() {
+		tooltip.els = {};
 		tooltip.vars = {};
+		tooltip.els.$document = $( document );
 		tooltip.vars.selector = '[data-tippy-content]';
 	},
 	// Execute callback after the DOM is loaded.
 	ready() {
 		tooltip.cache();
 		tooltip.initTooltip();
+		tooltip.ajaxTrigger();
 	},
 	// Initialize tooltip.
 	initTooltip() {
 		tippy( tooltip.vars.selector, {
 			arrow: true,
 		} );
+	},
+	// Re-initialize tooltip on Ajax event(s) being triggered.
+	ajaxTrigger() {
+		tooltip.els.$document.on( 'updated_cart_totals', tooltip.initTooltip );
 	},
 };
