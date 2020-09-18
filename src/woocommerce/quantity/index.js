8a80a33e9625df11f17317d882dbd423c6b8c8c7
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-18 21:30:56 +0300

    Handle plus (+) and minus (-) quantity button clicks

---
+++ src/woocommerce/quantity/index.js
@@ -0,0 +1,72 @@
+/**
+ * External dependencies
+ */
+import $ from 'jquery';
+import numeric from 'jquery.numeric'; /* eslint-disable-line no-unused-vars */
+import { gte, gt, lte, isEmpty, isNull, isEqual, isNaN, isUndefined } from 'lodash';
+
+export const quantity = {
+	cache() {
+		quantity.els = {};
+		quantity.vars = {};
+		quantity.vars.buttons = '.quantity button';
+		quantity.els.$body = $( 'body' );
+		quantity.els.$elm = $( '.quantity' );
+	},
+	// Execute callback after the DOM is loaded.
+	ready() {
+		quantity.cache();
+		quantity.onClick();
+		quantity.forceNumeric();
+	},
+	// Handles plus (+) and minus (-) button clicks.
+	onClick() {
+		quantity.els.$body.on( 'click', quantity.vars.buttons, ( event ) => {
+			const $this = $( event.target ),
+				$input = $this.closest( 'div' ).find( 'input.qty' );
+
+			let value = parseFloat( $input.val() ),
+				min = parseFloat( $input.attr( 'min' ) ),
+				max = parseFloat( $input.attr( 'max' ) ),
+				step = $input.attr( 'step' );
+
+			if ( isNull( min ) || isNaN( min ) ) min = 0;
+			if ( isNull( max ) || isNaN( max ) ) max = '';
+			if ( ! value || isNull( value ) || isNaN( value ) ) value = 0;
+			if ( isEqual( step, 'any' ) || isEmpty( step ) || isUndefined( step ) || isNaN( parseFloat( step ) ) )
+				step = 1;
+
+			// Add to the existing value.
+			if ( $this.hasClass( 'icon--plus' ) ) {
+				if ( max && gte( value, max ) ) {
+					$input.val( max );
+				} else {
+					$input.val( ( value + parseFloat( step ) ).toFixed( quantity._getDecimals( step ) ) );
+				}
+				// Subtract from the existing value.
+			} else if ( $this.hasClass( 'icon--minus' ) ) {
+				if ( min && lte( value, min ) ) {
+					$input.val( min );
+				} else if ( gt( value, 0 ) ) {
+					$input.val( ( value - parseFloat( step ) ).toFixed( quantity._getDecimals( step ) ) );
+				}
+			}
+
+			$input.trigger( 'change' ).trigger( 'qtyUpdated' );
+		} );
+	},
+	// Allows only valid numbers to be typed into the quantity field.
+	forceNumeric() {
+		quantity.els.$elm.find( 'input.qty' ).each( ( index, elm ) => {
+			$( elm ).numeric( { negative: false } );
+		} );
+	},
+	// Extract decimal points from a float number.
+	_getDecimals( number ) {
+		const match = ( '' + number ).match( /(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/ );
+		if ( ! match ) {
+			return 0;
+		}
+		return Math.max( 0, ( match[ 1 ] ? match[ 1 ].length : 0 ) - ( match[ 2 ] ? +match[ 2 ] : 0 ) );
+	},
+};
