6d165b84d294246ece8fc1c13976c69f411931ea
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-18 20:35:18 +0300

    Added font-formats option to the `fontpath` plugin

--- postcss.config.js
+++ postcss.config.js
@@ -27,7 +27,9 @@ module.exports = ( { env } ) => ( {
 		'postcss-selector-not': {},
 		'postcss-combine-media-query': {},
 		'postcss-normalize': {},
-		'postcss-fontpath': {},
+		'postcss-fontpath': {
+			formats: [ { type: 'woff2', ext: 'woff2' } ],
+		},
 		autoprefixer: {},
 	},
 } );
