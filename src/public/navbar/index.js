40652511a5be3da2ea7aa088f56e8056a2ad00af
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-19 01:22:33 +0300

    Initialize handheld (mobile) menu

--- src/public/navbar/index.js
+++ src/public/navbar/index.js
@@ -4,6 +4,7 @@
  * External dependencies
  */
 import $ from 'jquery';
+import slicknav from 'slicknav/dist/jquery.slicknav.min.js'; // eslint-disable-line no-unused-vars
 
 export const navbar = {
 	cache() {
@@ -11,11 +12,13 @@ export const navbar = {
 		navbar.vars = {};
 		navbar.vars.elm = 'site-navigation';
 		navbar.els.$submenu = $( `#${ navbar.vars.elm } .menu-item-has-children` );
+		navbar.els.$handheld = $( '.site-handheld-menu' );
 	},
 	// Execute callback after the DOM is loaded.
 	ready() {
 		navbar.cache();
 		navbar.isOffScreen();
+		navbar.handheldMenu();
 	},
 	// Avoid sub-menus from going off-screen.
 	isOffScreen() {
@@ -38,6 +41,18 @@ export const navbar = {
 					}, 300 );
 				}
 			}
+		} );
+	},
+	// Initialize handheld (mobile) menu.
+	handheldMenu() {
+		// Bail early, in case the menu is not present on the page.
+		if ( !!! navbar.els.$handheld.length ) return;
+
+		navbar.els.$handheld.slicknav( {
+			label: '',
+			duplicate: false,
+			allowParentLinks: false,
+			prependTo: '#masthead',
 		} );
 	},
 };
