8a9736093d5d5b1340c98905c2f0d26fe9cebb44
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-18 20:34:55 +0300

    Imported `Tooltip` script

--- src/public/index.js
+++ src/public/index.js
@@ -14,6 +14,7 @@ import './../vendor';
 import { buttons } from './buttons';
 import { navbar } from './navbar';
 import { skipLinkFocus } from './skip-link-focus';
+import { tooltip } from './tooltip';
 
 /**
  * External dependencies
@@ -22,7 +23,7 @@ import forEach from 'lodash/forEach';
 import domReady from '@wordpress/dom-ready';
 
 domReady( () => {
-	forEach( [ buttons, navbar, skipLinkFocus ], ( component ) => {
+	forEach( [ buttons, navbar, skipLinkFocus, tooltip ], ( component ) => {
 		component.ready();
 	} );
 } );
