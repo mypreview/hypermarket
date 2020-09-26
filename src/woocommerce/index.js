311d8dbf4a63191d1a88fffef40dd6f3a104fe58
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-26 19:04:00 +0300

    Imported `stickyAddToCart` script

--- src/woocommerce/index.js
+++ src/woocommerce/index.js
@@ -9,6 +9,7 @@ import './style.css';
 import { cart } from './cart';
 import { handheld } from './handheld';
 import { quantity } from './quantity';
+import { stickyAddToCart } from './sticky-add-to-cart';
 
 /**
  * External dependencies
@@ -17,7 +18,7 @@ import forEach from 'lodash/forEach';
 import domReady from '@wordpress/dom-ready';
 
 domReady( () => {
-	forEach( [ cart, handheld, quantity ], ( component ) => {
+	forEach( [ cart, handheld, quantity, stickyAddToCart ], ( component ) => {
 		component.ready();
 	} );
 } );
