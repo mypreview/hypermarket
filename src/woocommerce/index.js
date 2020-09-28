cf06ace7a361405aabeda25aad03b3bfe1e76ddc
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-28 16:30:46 +0300

    Imported `toast` script

--- src/woocommerce/index.js
+++ src/woocommerce/index.js
@@ -10,6 +10,7 @@ import { cart } from './cart';
 import { handheld } from './handheld';
 import { quantity } from './quantity';
 import { stickyAddToCart } from './sticky-add-to-cart';
+import { toast } from './toast';
 
 /**
  * External dependencies
@@ -18,7 +19,7 @@ import forEach from 'lodash/forEach';
 import domReady from '@wordpress/dom-ready';
 
 domReady( () => {
-	forEach( [ cart, handheld, quantity, stickyAddToCart ], ( component ) => {
+	forEach( [ cart, handheld, quantity, stickyAddToCart, toast ], ( component ) => {
 		component.ready();
 	} );
 } );
