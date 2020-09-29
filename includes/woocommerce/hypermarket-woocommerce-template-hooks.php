88e5b08785e57fd11601ceaf77dee00b704e9179
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-30 02:23:16 +0300

    Removed unused hooks

--- includes/woocommerce/hypermarket-woocommerce-template-hooks.php
+++ includes/woocommerce/hypermarket-woocommerce-template-hooks.php
@@ -50,8 +50,6 @@ remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wr
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end' );
 remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar' );
 remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination' );
-remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
-remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
 add_action( 'woocommerce_before_main_content', 'hypermarket_before_content' );
 add_action( 'woocommerce_after_main_content', 'hypermarket_after_content' );
 add_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 30 );
