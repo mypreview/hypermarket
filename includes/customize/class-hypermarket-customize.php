1af012998be62383751a7a0809aebfbaeebd0b56
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-09-30 02:26:05 +0300

    Registered `Product pagination` and `Sticky add to cart` checkbox controls

--- includes/customize/class-hypermarket-customize.php
+++ includes/customize/class-hypermarket-customize.php
@@ -23,6 +23,14 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 	final class Hypermarket_Customize {
 
 		/**
+		 * The settings control id.
+		 *
+		 * @access  public
+		 * @var     string      $settings       The blog name.
+		 */
+		public static $setting_prefix = 'hypermarket_customize';
+
+		/**
 		 * Setup class.
 		 *
 		 * @since   2.0.0
@@ -66,6 +74,8 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 			$this->_register_controls( $wp_customize, 'color', 'WP_Customize_Color_Control' );
 			// Registers Customizer font controls.
 			$this->_register_controls( $wp_customize, 'font', 'Hypermarket_Customize_Range_Control' );
+			// Registers Customizer layout controls.
+			$this->_register_controls( $wp_customize, 'layout', 'WP_Customize_Control' );
 
 			do_action( 'hypermarket_customize_register_controls', $wp_customize );
 		}
@@ -78,8 +88,7 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 		 */
 		public static function get_default_settings() {
 			global $hypermarket;
-			$setting_prefix = 'hypermarket_customize';
-			$font_attrs     = apply_filters(
+			$font_attrs = apply_filters(
 				'hypermarket_customize_font_attrs',
 				array(
 					'min'  => 1,
@@ -87,94 +96,96 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 					'step' => 1,
 				) 
 			);
-			$settings       = apply_filters(
+			$settings   = apply_filters(
 				'hypermarket_default_customize_values',
 				array(
 					'color' => array(
+						'has_css'     => true,
+						'type'        => 'color',
 						'title'       => __( 'Colors', 'hypermarket' ),
 						'description' => __( 'This option allows you to choose a color, view color suggestions, refine with the color picker and apply background color changes.', 'hypermarket' ),
 						'settings'    => array( 
 							array(
-								'id'       => sprintf( '%s_general_colors', $setting_prefix ),
+								'id'       => sprintf( '%s_general_colors', self::$setting_prefix ),
 								'title'    => esc_html__( 'General', 'hypermarket' ),
 								'controls' => array(
 									array(
 										'var'     => sprintf( '%s-general-text', $hypermarket->slug ),
-										'id'      => sprintf( '%s_general_text_color', $setting_prefix ),
+										'id'      => sprintf( '%s_general_text_color', self::$setting_prefix ),
 										'label'   => esc_html__( 'Text', 'hypermarket' ),
 										'default' => '#606060',
 									),
 									array(
 										'var'         => sprintf( '%s-general-text-alt', $hypermarket->slug ),
-										'id'          => sprintf( '%s_general_text_alt_color', $setting_prefix ),
+										'id'          => sprintf( '%s_general_text_alt_color', self::$setting_prefix ),
 										'label'       => esc_html__( 'Text', 'hypermarket' ),
 										'description' => esc_html__( 'Alternate', 'hypermarket' ),
 										'default'     => '#999999',
 									),
 									array(
 										'var'     => sprintf( '%s-general-quote', $hypermarket->slug ),
-										'id'      => sprintf( '%s_general_quote_color', $setting_prefix ),
+										'id'      => sprintf( '%s_general_quote_color', self::$setting_prefix ),
 										'label'   => esc_html__( 'Blockquote', 'hypermarket' ),
 										'default' => '#333333',
 									),
 									array(
 										'var'     => sprintf( '%s-general-primary', $hypermarket->slug ),
-										'id'      => sprintf( '%s_general_primary_color', $setting_prefix ),
+										'id'      => sprintf( '%s_general_primary_color', self::$setting_prefix ),
 										'label'   => esc_html__( 'Primary', 'hypermarket' ),
 										'default' => '#77cde3',
 									),
 									array(
 										'var'         => sprintf( '%s-general-primary-alt', $hypermarket->slug ),
-										'id'          => sprintf( '%s_general_primary_alt_color', $setting_prefix ),
+										'id'          => sprintf( '%s_general_primary_alt_color', self::$setting_prefix ),
 										'label'       => esc_html__( 'Primary', 'hypermarket' ),
 										'description' => esc_html__( 'Alternate', 'hypermarket' ),
 										'default'     => '#51bfdb',
 									),
 									array(
 										'var'     => sprintf( '%s-general-border', $hypermarket->slug ),
-										'id'      => sprintf( '%s_general_border_color', $setting_prefix ),
+										'id'      => sprintf( '%s_general_border_color', self::$setting_prefix ),
 										'label'   => esc_html__( 'Border', 'hypermarket' ),
 										'default' => '#ededed',
 									),
 									array(
 										'var'     => sprintf( '%s-general-tile', $hypermarket->slug ),
-										'id'      => sprintf( '%s_general_tile_color', $setting_prefix ),
+										'id'      => sprintf( '%s_general_tile_color', self::$setting_prefix ),
 										'label'   => esc_html__( 'Tile', 'hypermarket' ),
 										'default' => '#f5f5f5',
 									),
 									array(
 										'var'     => sprintf( '%s-general-background', $hypermarket->slug ),
-										'id'      => sprintf( '%s_general_background_color', $setting_prefix ),
+										'id'      => sprintf( '%s_general_background_color', self::$setting_prefix ),
 										'label'   => esc_html__( 'Background', 'hypermarket' ),
 										'default' => '#ffffff',
 									),
 								),
 							),
 							array(
-								'id'       => sprintf( '%s_alert_colors', $setting_prefix ),
+								'id'       => sprintf( '%s_alert_colors', self::$setting_prefix ),
 								'title'    => esc_html__( 'Alert', 'hypermarket' ),
 								'controls' => array(
 									array(
 										'var'     => sprintf( '%s-alert-info', $hypermarket->slug ),
-										'id'      => sprintf( '%s_alert_info_color', $setting_prefix ),
+										'id'      => sprintf( '%s_alert_info_color', self::$setting_prefix ),
 										'label'   => esc_html__( 'Info', 'hypermarket' ),
 										'default' => '#93c4ef',
 									),
 									array(
 										'var'     => sprintf( '%s-alert-success', $hypermarket->slug ),
-										'id'      => sprintf( '%s_alert_success_color', $setting_prefix ),
+										'id'      => sprintf( '%s_alert_success_color', self::$setting_prefix ),
 										'label'   => esc_html__( 'Success', 'hypermarket' ),
 										'default' => '#a7c04d',
 									),
 									array(
 										'var'     => sprintf( '%s-alert-warning', $hypermarket->slug ),
-										'id'      => sprintf( '%s_alert_warning_color', $setting_prefix ),
+										'id'      => sprintf( '%s_alert_warning_color', self::$setting_prefix ),
 										'label'   => esc_html__( 'Warning', 'hypermarket' ),
 										'default' => '#f8c313',
 									),
 									array(
 										'var'     => sprintf( '%s-alert-danger', $hypermarket->slug ),
-										'id'      => sprintf( '%s_alert_danger_color', $setting_prefix ),
+										'id'      => sprintf( '%s_alert_danger_color', self::$setting_prefix ),
 										'label'   => esc_html__( 'Danger', 'hypermarket' ),
 										'default' => '#ef0568',
 									),
@@ -183,16 +194,18 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 						),
 					),
 					'font' => array(
+						'has_css'     => true,
+						'type'        => 'range',
 						'title'       => __( 'Fonts', 'hypermarket' ),
-						'description' => __( '...', 'hypermarket' ),
+						'description' => __( 'Implies the font size changes to both site front-end and editor.', 'hypermarket' ),
 						'settings'    => array( 
 							array(
-								'id'       => sprintf( '%s_general_fonts', $setting_prefix ),
+								'id'       => sprintf( '%s_general_fonts', self::$setting_prefix ),
 								'title'    => esc_html__( 'General', 'hypermarket' ),
 								'controls' => array(
 									array(
 										'var'     => sprintf( '%s-general-small', $hypermarket->slug ),
-										'id'      => sprintf( '%s_general_small_font', $setting_prefix ),
+										'id'      => sprintf( '%s_general_small_font', self::$setting_prefix ),
 										'label'   => esc_html__( 'Small', 'hypermarket' ),
 										'default' => 14,
 										'suffix'  => 'px',
@@ -200,7 +213,7 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 									),
 									array(
 										'var'     => sprintf( '%s-general-normal', $hypermarket->slug ),
-										'id'      => sprintf( '%s_general_normal_font', $setting_prefix ),
+										'id'      => sprintf( '%s_general_normal_font', self::$setting_prefix ),
 										'label'   => esc_html__( 'Normal', 'hypermarket' ),
 										'default' => 16,
 										'suffix'  => 'px',
@@ -208,7 +221,7 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 									),
 									array(
 										'var'     => sprintf( '%s-general-medium', $hypermarket->slug ),
-										'id'      => sprintf( '%s_general_medium_font', $setting_prefix ),
+										'id'      => sprintf( '%s_general_medium_font', self::$setting_prefix ),
 										'label'   => esc_html__( 'Medium', 'hypermarket' ),
 										'default' => 23,
 										'suffix'  => 'px',
@@ -216,7 +229,7 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 									),
 									array(
 										'var'     => sprintf( '%s-general-large', $hypermarket->slug ),
-										'id'      => sprintf( '%s_general_large_font', $setting_prefix ),
+										'id'      => sprintf( '%s_general_large_font', self::$setting_prefix ),
 										'label'   => esc_html__( 'Large', 'hypermarket' ),
 										'default' => 26,
 										'suffix'  => 'px',
@@ -224,11 +237,36 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 									),
 									array(
 										'var'     => sprintf( '%s-general-huge', $hypermarket->slug ),
-										'id'      => sprintf( '%s_general_huge_font', $setting_prefix ),
+										'id'      => sprintf( '%s_general_huge_font', self::$setting_prefix ),
 										'label'   => esc_html__( 'Huge', 'hypermarket' ),
 										'default' => 37,
 										'suffix'  => 'px',
 										'attrs'   => $font_attrs,
+									),
+								),
+							),
+						),
+					),
+					'layout' => array(
+						'title'       => __( 'Layout', 'hypermarket' ),
+						'description' => __( 'This section gives you creative control of style and layout options for your theme.', 'hypermarket' ),
+						'settings'    => array(
+							array(
+								'id'             => sprintf( '%s_wc_layout', self::$setting_prefix ),
+								'title'          => esc_html__( 'WooCommerce', 'hypermarket' ),
+								'theme_supports' => 'woocommerce',
+								'controls'       => array(
+									array(
+										'type'           => 'checkbox',
+										'id'             => sprintf( '%s_wc_single_product_pagination', self::$setting_prefix ),
+										'label'          => esc_html__( 'Product pagination', 'hypermarket' ),
+										'description'    => esc_html__( 'Previous and next product navigation buttons from same collection on product page.', 'hypermarket' ),
+									),
+									array(
+										'type'           => 'checkbox',
+										'id'             => sprintf( '%s_wc_sticky_single_add_to_cart', self::$setting_prefix ),
+										'label'          => esc_html__( 'Sticky add to cart', 'hypermarket' ),
+										'description'    => esc_html__( 'A content bar at the top of the browser window which includes the product name, price, rating, stock status and the add to cart button.', 'hypermarket' ),
 									),
 								),
 							),
@@ -265,7 +303,13 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 		 */
 		public static function get_css() {
 			$return = ':root {';
-			$groups = self::get_controls();
+			$groups = array_filter(
+				self::get_controls(),
+				// phpcs:ignore PHPCompatibility.FunctionDeclarations.NewClosure.Found
+				function( $controls ) { 
+					return isset( $controls['has_css'] ); 
+				}
+			);
 
 			if ( is_array( $groups ) && ! empty( $groups ) ) {
 				// Loop through Customize groups.
@@ -306,7 +350,6 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 		 * @param   string               $id             The group id or key name.
 		 * @param   string               $class          Theme Customizer control class name.
 		 * @return  void
-		 * @phpcs:disable PSR2.Methods.MethodDeclaration.Underscore
 		 */
 		private function _register_controls( $hm_customize, $id, $class ) {
 			global $hypermarket;
@@ -317,6 +360,7 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 			// Check whether there are any controls to register.
 			if ( is_array( $group ) && ! empty( $group ) && isset( $group['settings'] ) ) {
 				$group_title       = isset( $group['title'] ) ? (string) $group['title'] : '';
+				$group_type        = isset( $group['type'] ) ? (string) $group['type'] : '';
 				$group_description = isset( $group['description'] ) ? (string) $group['description'] : '';
 				$group_priority    = isset( $group['priority'] ) ? (int) $group['priority'] : 30;
 
@@ -338,17 +382,19 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 				foreach ( $controls as $section ) {
 					// Determine if the section id is declared and is different than null.
 					if ( isset( $section['id'] ) ) {
-						$section_id       = (string) $section['id'];
-						$section_title    = (string) $section['title'];
-						$section_controls = (array) $section['controls'];
+						$section_id             = (string) $section['id'];
+						$section_title          = (string) $section['title'];
+						$section_theme_supports = isset( $control['theme_supports'] ) ? (string) $control['theme_supports'] : '';
+						$section_controls       = (array) $section['controls'];
 
 						// Registers a new customize section.
 						$hm_customize->add_section(
 							esc_html( $section_id ),
 							array(
-								'title'      => esc_html( $section_title ),
-								'panel'      => esc_html( $panel_id ),
-								'capability' => esc_html( $capability ),
+								'title'          => esc_html( $section_title ),
+								'theme_supports' => esc_html( $section_theme_supports ),
+								'panel'          => esc_html( $panel_id ),
+								'capability'     => esc_html( $capability ),
 							) 
 						);
 
@@ -358,6 +404,7 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 								// Determine if the control id is declared and is different than null.
 								if ( isset( $control['id'] ) ) {
 									$control_id            = (string) $control['id'];
+									$control_type          = isset( $control['type'] ) ? (string) $control['type'] : (string) $group_type;
 									$control_label         = isset( $control['label'] ) ? (string) $control['label'] : '';
 									$control_description   = isset( $control['description'] ) ? (string) $control['description'] : '';
 									$control_default_value = isset( $control['default'] ) ? (string) $control['default'] : '';
@@ -371,8 +418,8 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 											'transport'  => 'refresh',
 											'capability' => 'edit_theme_options',
 											// phpcs:ignore WordPress.NamingConventions.ValidHookName.UseUnderscores, WordPress.NamingConventions.PrefixAllGlobals.DynamicHooknameFound
-											'default'    => apply_filters( sprintf( '%s_default', hypermarket_sanitize( $control_id ) ), hypermarket_sanitize( $control_default_value, $group ) ),
-											'sanitize_callback' => hypermarket_sanitize_method( $group ),
+											'default'    => apply_filters( sprintf( '%s_default', hypermarket_sanitize( $control_id ) ), hypermarket_sanitize( $control_default_value, $control_type ) ),
+											'sanitize_callback' => hypermarket_sanitize_method( $control_type ),
 										) 
 									);
 									
@@ -382,6 +429,7 @@ if ( ! class_exists( 'Hypermarket_Customize' ) ) :
 											$hm_customize,
 											esc_html( $control_id ),
 											array(
+												'type'     => esc_html( $control_type ),
 												'label'    => esc_html( $control_label ),
 												'description' => esc_html( $control_description ),
 												'section'  => esc_html( $section_id ),
