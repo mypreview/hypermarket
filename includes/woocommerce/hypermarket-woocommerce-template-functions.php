0a8f66fcf6ff4bf592afe01c9ddde7c0fc98477e
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-10-02 02:47:37 +0300

    Display the "new" flash (badge)

--- includes/woocommerce/hypermarket-woocommerce-template-functions.php
+++ includes/woocommerce/hypermarket-woocommerce-template-functions.php
@@ -442,6 +442,44 @@ if ( ! function_exists( 'hypermarket_myaccount_user_info' ) ) :
 	}
 endif;
 
+if ( ! function_exists( 'hypermarket_product_new_flash' ) ) :
+	/**
+	 * Display the "new" flash (badge).
+	 *
+	 * @since   2.0.0
+	 * @param   bool $ignore_sale    Optional. Whether to bypass the on_sale status.
+	 * @param   bool $echo           Optional. Echo the string or return it.
+	 * @return  void|html
+	 */
+	function hypermarket_product_new_flash( $ignore_sale = false, $echo = true ) {
+		global $product;
+
+		// Retrieves theme modification value for the current theme (parent or child).
+		$is_activated = get_theme_mod( sprintf( '%s_wc_catalog_new_flash', Hypermarket_Customize::$setting_prefix ), false );
+
+		if ( ( $product->is_on_sale() && ! $ignore_sale ) || ! $is_activated ) {
+			return;
+		}
+
+		$return            = '';
+		$written_date      = get_the_time( 'Y-m-d', $product->get_id() );
+		$written_timestamp = strtotime( $written_date );
+		$newness           = apply_filters( 'hypermarket_wc_catalog_new_flash_days', 30 );
+
+		// If the product was published within the newness time frame display the new flash (badge).
+		if ( $newness > 0 && ( time() - ( 60 * 60 * 24 * $newness ) ) < $written_timestamp ) {
+			/* translators: 1: Open span tag, 2: Close span tag. */
+			$return = sprintf( esc_html_x( '%1$sNew%2$s', 'new flash', 'hypermarket' ), '<span class="onnew">', '</span>' );
+		}
+
+		if ( ! $echo ) {
+			return $return;
+		}
+
+		echo $return; //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	}
+endif;
+
 if ( ! function_exists( 'hypermarket_product_categories' ) ) :
 	/**
 	 * Append the product categories to archive products loop.
