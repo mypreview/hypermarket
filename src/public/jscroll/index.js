97deed42b45227f9c5c68e20e7afa3dd065b4c22
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-10-03 02:29:30 +0300

    Initialize jScroll

---
+++ src/public/jscroll/index.js
@@ -0,0 +1,33 @@
+/**
+ * External dependencies
+ */
+import $ from 'jquery';
+import jscroll from 'jscroll'; /* eslint-disable-line no-unused-vars */
+
+export const jScroll = {
+	cache() {
+		jScroll.els = {};
+		jScroll.vars = {};
+		jScroll.vars.selector = 'jscroll-div';
+		jScroll.vars.$selector = $( `.${ jScroll.vars.selector }` );
+		jScroll.vars.inner = `.${ jScroll.vars.selector }__inner`;
+		jScroll.vars.next = 'a.next';
+	},
+	// Execute callback after the DOM is loaded.
+	ready() {
+		jScroll.cache();
+		jScroll.init();
+	},
+	// Initialize jScroll.
+	init() {
+		// Bail early, in case the menu is not present on the page.
+		if ( !!! jScroll.vars.$selector.length ) return;
+
+		jScroll.vars.$selector.jscroll( {
+			autoTrigger: false,
+			loadingHtml: `<div class="${ jScroll.vars.selector }__loading">LOADING</div>`,
+			nextSelector: jScroll.vars.next,
+			contentSelector: jScroll.vars.inner,
+		} );
+	},
+};
