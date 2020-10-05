e72c41867ad40d46beb9becc171ec283a8cdb1f6
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-10-05 18:08:50 +0300

    Added customize `More` section hook

--- includes/hypermarket-template-hooks.php
+++ includes/hypermarket-template-hooks.php
@@ -134,3 +134,10 @@ add_action( 'hypermarket_archive_top', 'hypermarket_archive_header' );
 add_action( 'hypermarket_archive_top', 'hypermarket_div_close', 25 );
 add_action( 'hypermarket_home_top', 'hypermarket_jscroll', 30 );
 add_action( 'hypermarket_home_bottom', 'hypermarket_jscroll_close', 5 );
+
+/**
+ * Customize
+ *
+ * @see  hypermarket_customize_more_section()
+ */
+add_action( 'hypermarket_customize_register_controls', 'hypermarket_customize_more_section' );
