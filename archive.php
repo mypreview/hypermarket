eaae596d30e9b4c786a6d9d8305008c599a14774
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-10-08 03:36:05 +0300

    Adjusted template hooks

--- archive.php
+++ archive.php
@@ -14,42 +14,58 @@ get_header();
 
 ?><div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<?php
-		
-		/**
-		 * Functions hooked into `hypermarket_archive_top` action
-		 *
-		 * @hooked  hypermarket_div                 - 5
-		 * @hooked  hypermarket_archive_header      - 10
-		 * @hooked  hypermarket_breadcrumb          - 20
-		 * @hooked  hypermarket_div_close           - 25
-		 * @hooked  hypermarket_jscroll             - 30
-		 */
-		do_action( 'hypermarket_archive_top' );
+			<?php
 
-		if ( have_posts() ) :
-			get_template_part( 'loop' );
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-		endif;
-		
-		/**
-		 * Functions hooked into `hypermarket_archive_bottom` action
-		 *
-		 * @hooked  hypermarket_jscroll_close       - 5
-		 */
-		do_action( 'hypermarket_archive_bottom' );
+				/**
+				 * Functions hooked into `hypermarket_archive_top` action
+				 *
+				 * @hooked  hypermarket_div                    - 5
+				 * @hooked  hypermarket_posts_page_header      - 10
+				 * @hooked  hypermarket_breadcrumb             - 20
+				 * @hooked  hypermarket_div_close              - 25
+				 */
+				do_action( 'hypermarket_archive_top' );
 
-		?>
+			?>
+			<div class="entry-wrapper">
+				<?php
+				
+				/**
+				 * Functions hooked into `hypermarket_before_loop` action
+				 *
+				 * @hooked  hypermarket_jscroll                - 10
+				 */
+				do_action( 'hypermarket_before_loop' );
+				
+				if ( have_posts() ) :
+					get_template_part( 'loop' );
+				else :
+					get_template_part( 'template-parts/content', 'none' );
+				endif;
+				
+				/**
+				 * Functions hooked into `hypermarket_after_loop` action
+				 *
+				 * @hooked  hypermarket_jscroll_close          - 10
+				 */
+				do_action( 'hypermarket_after_loop' );
+
+				?>
+			</div>
+			<?php
+
+			/**
+			 * Functions hooked into `hypermarket_sidebar` action
+			 *
+			 * @hooked  hypermarket_get_sidebar               - 10
+			 */
+			do_action( 'hypermarket_sidebar' );
+
+			do_action( 'hypermarket_archive_bottom' );
+			
+			?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php
-
-/**
- * Functions hooked into `hypermarket_sidebar` action
- *
- * @hooked  hypermarket_get_sidebar         - 10
- */
-do_action( 'hypermarket_sidebar' );
 
 get_footer();
--- index.php
+++ index.php
@@ -19,42 +19,58 @@ get_header();
 
 ?><div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<?php
-		
-		/**
-		 * Functions hooked into `hypermarket_home_top` action
-		 *
-		 * @hooked  hypermarket_div                    - 5
-		 * @hooked  hypermarket_posts_page_header      - 10
-		 * @hooked  hypermarket_breadcrumb             - 20
-		 * @hooked  hypermarket_div_close              - 25
-		 * @hooked  hypermarket_jscroll           	   - 30
-		 */
-		do_action( 'hypermarket_home_top' );
-		
-		if ( have_posts() ) :
-			get_template_part( 'loop' );
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-		endif;
-		
-		/**
-		 * Functions hooked into `hypermarket_home_bottom` action
-		 *
-		 * @hooked  hypermarket_jscroll_close          - 5
-		 */
-		do_action( 'hypermarket_home_bottom' );
+			<?php
 
-		?>
+				/**
+				 * Functions hooked into `hypermarket_home_top` action
+				 *
+				 * @hooked  hypermarket_div                    - 5
+				 * @hooked  hypermarket_posts_page_header      - 10
+				 * @hooked  hypermarket_breadcrumb             - 20
+				 * @hooked  hypermarket_div_close              - 25
+				 */
+				do_action( 'hypermarket_home_top' );
+
+			?>
+			<div class="entry-wrapper">
+				<?php
+				
+				/**
+				 * Functions hooked into `hypermarket_before_loop` action
+				 *
+				 * @hooked  hypermarket_jscroll           	   - 10
+				 */
+				do_action( 'hypermarket_before_loop' );
+				
+				if ( have_posts() ) :
+					get_template_part( 'loop' );
+				else :
+					get_template_part( 'template-parts/content', 'none' );
+				endif;
+				
+				/**
+				 * Functions hooked into `hypermarket_after_loop` action
+				 *
+				 * @hooked  hypermarket_jscroll_close          - 10
+				 */
+				do_action( 'hypermarket_after_loop' );
+
+				?>
+			</div>
+			<?php
+
+			/**
+			 * Functions hooked into `hypermarket_sidebar` action
+			 *
+			 * @hooked  hypermarket_get_sidebar          	  - 10
+			 */
+			do_action( 'hypermarket_sidebar' );
+
+			do_action( 'hypermarket_home_bottom' );
+
+			?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php
-
-/**
- * Functions hooked into `hypermarket_sidebar` action
- *
- * @hooked  hypermarket_get_sidebar         - 10
- */
-do_action( 'hypermarket_sidebar' );
 
 get_footer();
