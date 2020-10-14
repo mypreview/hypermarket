22e2320f5211889fd1a0901117b73f430debbb8e
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-10-15 02:28:56 +0300

    Updated template structure

--- search.php
+++ search.php
@@ -14,36 +14,58 @@ get_header();
 
 ?><div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<?php 
-
-		if ( have_posts() ) : 
-
-			?>
-			<header class="page-header">
-				<h1 class="page-title">
-				<?php
-					/* translators: %s: search term */
-					printf( esc_attr__( 'Search Results for: %s', 'hypermarket' ), sprintf( '<span>%s</span>', get_search_query() ) );
-				?>
-				</h1>
-			</header><!-- .page-header -->
 			<?php
 
-			get_template_part( 'loop' );
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-		endif;
-		
-		?>
+				/**
+				 * Functions hooked into `hypermarket_search_top` action
+				 *
+				 * @hooked  hypermarket_div                    - 5
+				 * @hooked  hypermarket_search_page_header     - 10
+				 * @hooked  hypermarket_breadcrumb             - 20
+				 * @hooked  hypermarket_div_close              - 25
+				 */
+				do_action( 'hypermarket_search_top' );
+
+			?>
+			<div class="entry-wrapper">
+				<?php
+				
+				/**
+				 * Functions hooked into `hypermarket_before_loop` action
+				 *
+				 * @hooked  hypermarket_jscroll                - 10
+				 */
+				do_action( 'hypermarket_before_loop' );
+				
+				if ( have_posts() ) :
+					get_template_part( 'loop' );
+				else :
+					get_template_part( 'template-parts/content', 'none' );
+				endif;
+				
+				/**
+				 * Functions hooked into `hypermarket_after_loop` action
+				 *
+				 * @hooked  hypermarket_jscroll_close          - 10
+				 */
+				do_action( 'hypermarket_after_loop' );
+
+				?>
+			</div>
+			<?php
+
+			/**
+			 * Functions hooked into `hypermarket_sidebar` action
+			 *
+			 * @hooked  hypermarket_get_sidebar               - 10
+			 */
+			do_action( 'hypermarket_sidebar' );
+
+			do_action( 'hypermarket_search_bottom' );
+
+			?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php
-
-/**
- * Functions hooked into `hypermarket_sidebar` action
- *
- * @hooked  hypermarket_get_sidebar         - 10
- */
-do_action( 'hypermarket_sidebar' );
 
 get_footer();
