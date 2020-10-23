033cc280bc392aaa6dc40770580922bb79babf64
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-10-24 02:43:34 +0300

    Admin facing hooks

---
+++ includes/admin/hypermarket-admin-template-hooks.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Hypermarket admin hooks.
+ *
+ * @link       https://mypreview.github.io/hypermarket
+ * @author     MyPreview
+ * @since      2.0.0
+ *
+ * @package    hypermarket
+ * @subpackage hypermarket/includes
+ */
+
+/**
+ * Header
+ *
+ * @see  hypermarket_welcome_header()
+ */
+add_action( 'hypermarket_welcome_top', 'hypermarket_welcome_header' );
+
+/**
+ * Content
+ *
+ * @see  hypermarket_welcome_tabs()
+ */
+add_action( 'hypermarket_welcome_content', 'hypermarket_welcome_tabs' );
