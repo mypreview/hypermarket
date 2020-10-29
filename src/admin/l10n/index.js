e4e7bd4f17b0a175b45bc67091a26209973e74f4
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-10-29 16:43:38 +0300

    Retrieve admin-facing l10n vars

---
+++ src/admin/l10n/index.js
@@ -0,0 +1,8 @@
+/* global hypermarket_admin */
+
+/**
+ * External dependencies
+ */
+import { has, isPlainObject } from 'lodash';
+
+export const l10n = ( path ) => ( isPlainObject( hypermarket_admin ) && has( hypermarket_admin, path ) ? hypermarket_admin[ path ] : '' );
