384d25a5c2d2882621e547844d4eb144605e755a
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-10-30 02:56:49 +0300

    Updated welcome tabs markup

--- includes/admin/hypermarket-admin-template-functions.php
+++ includes/admin/hypermarket-admin-template-functions.php
@@ -45,29 +45,64 @@ if ( ! function_exists( 'hypermarket_welcome_tabs' ) ) :
 	function hypermarket_welcome_tabs() {
 		$classname = (string) Hypermarket_Admin::$welcome_slug;
 		?>
-		<div class="nav-tab-wrapper <?php echo sanitize_html_class( $classname ); ?>__tabs">
-			<a href="#" class="nav-tab nav-tab-active" data-tab="welcome">
-				<?php echo esc_html_e( 'Welcome', 'hypermarket' ); ?>
-			</a>
-			<a href="#" class="nav-tab" data-tab="extensions">
-				<?php echo esc_html_e( 'Recommended Extensions', 'hypermarket' ); ?>
-			</a>
+		<div class="tabs nav-tab-wrapper <?php echo sanitize_html_class( $classname ); ?>__tabs" data-toggle="tabslet" data-animation="true" data-autorotate="false">
+			<ul class="tab-links">
+				<li>
+					<a href="#welcome" class="nav-tab nav-tab-active">
+						<?php echo esc_html_e( 'Welcome', 'hypermarket' ); ?>
+					</a>
+				</li>
+				<li>
+					<a href="#extensions" class="nav-tab">
+						<?php echo esc_html_e( 'Recommended Extensions', 'hypermarket' ); ?>
+					</a>
+				</li>
+			</ul>
+
+			<?php 
+			
+			/**
+			 * Functions hooked into `hypermarket_welcome_tab_content` action
+			 *
+			 * @hooked  hypermarket_welcome_tab            - 10
+			 * @hooked  hypermarket_extensions_tab         - 20
+			 */
+			do_action( 'hypermarket_welcome_tab_content' ); 
+			
+			?>
 		</div>
 		<?php
 	}
 endif;
 
-if ( ! function_exists( 'hypermarket_welcome_extensions' ) ) :
+if ( ! function_exists( 'hypermarket_welcome_tab' ) ) :
 	/**
 	 * Display `Recommended Extensions` tab content.
 	 *
 	 * @since   2.0.0
 	 * @return  void
 	 */
-	function hypermarket_welcome_extensions() {
+	function hypermarket_welcome_tab() {
 		$classname = (string) Hypermarket_Admin::$welcome_slug;
 		?>
-		<div class="<?php echo sanitize_html_class( $classname ); ?>__extensions">
+		<div id="welcome" class="<?php echo sanitize_html_class( $classname ); ?>__intro">
+			Welcome!
+		</div>
+		<?php
+	}
+endif;
+
+if ( ! function_exists( 'hypermarket_extensions_tab' ) ) :
+	/**
+	 * Display `Recommended Extensions` tab content.
+	 *
+	 * @since   2.0.0
+	 * @return  void
+	 */
+	function hypermarket_extensions_tab() {
+		$classname = (string) Hypermarket_Admin::$welcome_slug;
+		?>
+		<div id="extensions" class="<?php echo sanitize_html_class( $classname ); ?>__extensions">
 			<ul></ul>
 			<div class="spinner is-active alignleft"></div>
 		</div>
