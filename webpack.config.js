b59a24718a070415f06a475dcf501ea3cb0ccd96
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-05-15 14:59:33 +0300

    Removed `url-loader`

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@
  * for the "Hypermarket" theme reside in this file.
  *
  * @requires Webpack
- * @package
- * @author      MyPreview (Github: @mahdiyazdani, @mypreview)
+ * @author   MyPreview (Github: @mahdiyazdani, @mypreview)
  */
 const defaultConfig = require( './node_modules/@wordpress/scripts/config/webpack.config.js' );
 const path = require( 'path' );
@@ -14,47 +13,17 @@ const WebpackNotifierPlugin = require( 'webpack-notifier' );
 const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
 const ProgressBarPlugin = require( 'progress-bar-webpack-plugin' );
 const FixStyleOnlyEntriesPlugin = require( 'webpack-fix-style-only-entries' );
-const OptimizeCSSAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
 const LicenseCheckerWebpackPlugin = require( 'license-checker-webpack-plugin' );
 
 module.exports = {
 	...defaultConfig,
 	entry: {
-		'legacy-editor': path.resolve( process.cwd(), 'src', 'legacy-editor/style.css' ),
+		'legacy-editor': path.resolve( process.cwd(), 'src', 'legacy-editor/index.css' ),
 		admin: path.resolve( process.cwd(), 'src', 'admin/index.js' ),
 		editor: path.resolve( process.cwd(), 'src', 'editor/index.js' ),
 		public: path.resolve( process.cwd(), 'src', 'public/index.js' ),
 		customize: path.resolve( process.cwd(), 'src', 'customize/index.js' ),
 		woocommerce: path.resolve( process.cwd(), 'src', 'woocommerce/index.js' ),
-	},
-	module: {
-		...defaultConfig.module,
-		rules: [
-			...defaultConfig.module.rules,
-			{
-				test: /\.(png|jpg|gif)$/i,
-				use: {
-					loader: 'url-loader',
-					options: {
-						encoding: true,
-					},
-				},
-			},
-		],
-	},
-	optimization: {
-		...defaultConfig.optimization,
-		splitChunks: {
-			automaticNameDelimiter: '--',
-		},
-		minimizer: [
-			...defaultConfig.optimization.minimizer,
-			new OptimizeCSSAssetsPlugin( {
-				cssProcessorPluginOptions: {
-					preset: [ 'default', { discardComments: { removeAll: true } } ],
-				},
-			} ),
-		],
 	},
 	plugins: [
 		...defaultConfig.plugins,
@@ -63,8 +32,7 @@ module.exports = {
 			cleanStaleWebpackAssets: false,
 		} ),
 		new ProgressBarPlugin( {
-			format:
-				chalk.blue( 'Build core script' ) + ' [:bar] ' + chalk.green( ':percent' ) + ' :msg (:elapsed seconds)',
+			format: chalk.blue( 'Build' ) + ' [:bar] ' + chalk.green( ':percent' ) + ' :msg (:elapsed seconds)',
 		} ),
 		new WebpackRTLPlugin( {
 			filename: '[name]-rtl.css',
