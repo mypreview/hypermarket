4d21d2d196005629bea86a12449b4e10824e2c13
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-03-07 15:09:22 +0300

    FPO

--- src/woocommerce/index.js
+++ src/woocommerce/index.js
@@ -1,25 +1,4 @@
 /**
  * Stylesheet dependencies.
  */
-import './index.css';
-
-/**
- * Custom scripts.
- */
-import cart from './cart';
-import handheld from './handheld';
-import quantity from './quantity';
-import stickyAddToCart from './sticky-add-to-cart';
-import toast from './toast';
-
-/**
- * External dependencies
- */
-import forEach from 'lodash/forEach';
-import domReady from '@wordpress/dom-ready';
-
-domReady( () => {
-	forEach( [ cart, handheld, quantity, stickyAddToCart, toast ], ( component ) => {
-		component.ready();
-	} );
-} );
+import './index.scss';
