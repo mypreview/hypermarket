f581eb110262c11ce12245f216228db93cf3c5a1
Mahdi Yazdani <mahdiyazdani@msn.com>
2022-03-07 15:05:38 +0300

    Excluded new rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,40 +1,21 @@
 module.exports = {
 	extends: '@wordpress/stylelint-config/scss',
 	rules: {
-		'max-line-length': null,
-		'value-keyword-case': null,
-		'selector-id-pattern': null,
-		'no-duplicate-selectors': null,
-		'selector-class-pattern': null,
-		'selector-type-no-unknown': null,
-		'no-descending-specificity': null,
+		'at-rule-empty-line-before': null,
+		'at-rule-no-unknown': null,
 		'comment-empty-line-before': null,
 		'declaration-block-no-duplicate-properties': null,
+		'declaration-property-unit-allowed-list': null,
+		'font-weight-notation': null,
+		'max-line-length': null,
+		'no-descending-specificity': null,
+		'no-duplicate-selectors': null,
+		'rule-empty-line-before': null,
+		'selector-class-pattern': null,
+		'value-keyword-case': null,
+		'font-family-no-missing-generic-family-keyword': null,
+		'selector-id-pattern': null,
 		'scss/selector-no-redundant-nesting-selector': null,
-		'property-no-unknown': [
-			true,
-			{
-				ignoreProperties: [ 'box', 'box-item', 'border-right-radius', 'border-left-radius' ],
-			},
-		],
-		'at-rule-no-unknown': [
-			true,
-			{
-				ignoreAtRules: [ 'important', 'for', 'extend', 'each', 'mixin', 'define-mixin' ],
-			},
-		],
-		'scss/at-rule-no-unknown': [
-			true,
-			{
-				ignoreAtRules: [ 'important', 'define-mixin' ],
-			},
-		],
-		'selector-pseudo-element-no-unknown': [ true, { ignorePseudoElements: [ 'range-thumb' ] } ],
-		'font-family-no-missing-generic-family-keyword': [
-			true,
-			{
-				ignoreFontFamilies: [ 'dashicons' ],
-			},
-		],
+		'scss/no-global-function-names': null,
 	},
 };
